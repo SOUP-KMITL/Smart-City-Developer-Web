8ac2b8f93f910a5ebc4e6f6048c2a917bc4061a1
Jitiwattana <bbos1@windowslive.com>
2018-01-14 22:18:10 +0700

    ADD collapes navbar

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -6,33 +6,53 @@ import {
     NavItem,
     NavLink,
     NavbarBrand,
+    Collapse,
+    NavbarToggler,
 } from 'reactstrap';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 
 class MyNavbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
     render() {
         return (
             <div>
                 <Navbar color='faded' light expand='md' className='navbar'>
                     <NavbarBrand><img src={logo} width={150} alt='smart-city' /></NavbarBrand>
-                    <Nav className='ml-auto' navbar>
-                        <NavItem className='menu'>
-                            <Link to='/'><NavLink>Home</NavLink></Link>
-                        </NavItem>
-                        <NavItem className='menu'>
-                            <Link to='about'><NavLink>About</NavLink></Link>
-                        </NavItem>
-                        <NavItem className='menu'>
-                            <Link to='contact'><NavLink>Contact Us</NavLink></Link>
-                        </NavItem>
-                        <NavItem className='menu'>
-                            <Link to='market-place'><NavLink>Market Place</NavLink></Link>
-                        </NavItem>
-                        <NavItem className='menu'>
-                            <Link to='about'><NavLink>About Us</NavLink></Link>
-                        </NavItem>
-                    </Nav>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                        <Nav className='ml-auto text-center' navbar>
+                            <NavItem className='menu'>
+                                <Link to='/'><NavLink>Home</NavLink></Link>
+                            </NavItem>
+                            <NavItem className='menu'>
+                                <Link to='about'><NavLink>About</NavLink></Link>
+                            </NavItem>
+                            <NavItem className='menu'>
+                                <Link to='contact'><NavLink>Contact Us</NavLink></Link>
+                            </NavItem>
+                            <NavItem className='menu'>
+                                <Link to='market-place'><NavLink>Market Place</NavLink></Link>
+                            </NavItem>
+                            <NavItem className='menu'>
+                                <Link to='about'><NavLink>About Us</NavLink></Link>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
                 </Navbar>
             </div>
         );
