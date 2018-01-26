23338f4fade65d974525399a86a8756484b09de7
Jitiwattana <bbos1@windowslive.com>
2018-01-26 08:00:57 +0700

    ADD react-router-redux to fix bug state not change when router is changed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import App from './container/App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider }  from 'react-redux';
-import { createStore } from 'redux';
-import allReducers from './state/reducers.js';
+import { routerMiddleware, ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
-const store = createStore(allReducers);
+import registerServiceWorker from './registerServiceWorker';
+import allReducers from './state/reducers.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './container/App';
+
+const history = createHistory()
+const middleware = routerMiddleware(history)
+const store = createStore(allReducers, applyMiddleware(middleware));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
       <App />
-    </Provider>
-  </BrowserRouter>
+    </ConnectedRouter>
+  </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import reducer from './user.js';
+import { routerReducer } from 'react-router-redux'
 
 
 const allReducers = combineReducers({
+  router: routerReducer,
   userData: reducer,
 })
 
