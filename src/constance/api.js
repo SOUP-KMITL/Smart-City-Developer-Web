23532422c5d1e8c927cafce41803032734327cd2
Jitiwattana <bbos1@windowslive.com>
2018-01-27 05:21:53 +0700

    CHANGE signin api to basic auth (not test not done)

--- src/constance/api.js
+++ src/constance/api.js
@@ -3,8 +3,7 @@ const domain = 'https://api.smartcity.kmitl.io';
 const api = {
   cityService: domain + '/web/cityServices/',
   dataCollection: domain + '/web/dataCollections/',
-  signin: domain + '/web/login/',
-  getSignin: domain + '/api/v1/users/login/'
+  signin: domain + '/api/v1/users/login/'
 }
 
 export default api;
--- src/view/login/login.jsx
+++ src/view/login/login.jsx
@@ -27,16 +27,19 @@ class Login extends React.Component {
     requestLogin(value) {
         this.setState({ loading: true })
 
+        const auth = 'Basic ' + new Buffer(value.username + ':' + value.password).toString('base64');
+
         fetch(api.signin, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(value)
-        }).then((response) => response.json()).then(
+            method: 'GET',
+            headers: {
+                'Authorization': auth
+            }
+        }).then(response => response.json()).then(
             res => {
                 this.setState({ loginResult: false })
                 if (res.success != false) {
-                    Storage.saveAccessData(res);
-                    this.getUserData(value)
+                    Storage.saveUserData(res);
+                    this.props.updateUserData(res);
                 }
                 // Redirect
             },
@@ -49,26 +52,6 @@ class Login extends React.Component {
                 this.setState({ loading: false })
             }, 1000);
         })
-    }
-
-    getUserData(value) {
-        const { username, password } = value;
-        const auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-
-        fetch(api.getSignin, {
-            method: 'GET',
-            headers: {
-                'Authorization': auth
-            },
-        }).then(response => response.json()).then(
-            res => {
-                Storage.saveUserData(res);
-                this.props.updateUserData(res);
-            },
-            err => {
-                console.log('CANNOT GET USER DATA');
-            }
-        )
     }
 
 
