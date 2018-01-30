21375e901f170e90b66416275502ceaba995d35d
Jitiwattana <bbos1@windowslive.com>
2018-01-30 21:44:15 +0700

    FIX footer when mediascreen lower sm (768)

--- src/view/profile/profile.jsx
+++ src/view/profile/profile.jsx
@@ -23,46 +23,31 @@ class Profile extends React.Component {
 
 
     render() {
-        const { userId, firstName, lastName, email, userName } = this.props.userData;
-
         return (
             <Container className='fullscreen' style={{ paddingTop: 50 }}>
                 <Row>
+
                     <Col md={3} xs={12} sm={12}>
-                        <div className='profile-image'>
-                            <img
-                                src='https://pickaface.net/assets/images/slides/slide4.png'
-                                width='200px'
-                                className='img-fluid'
-                                alt=''
-                            />
-                        </div>
-                        <div className='profile-info'>
-                            <strong>@{userName}</strong>
-                            <p>{firstName} {lastName}</p>
-                        </div>
-                        <ListGroup>
-                            <ListGroupItem>Edit Profile</ListGroupItem>
-                            <ListGroupItem>My DataCollections</ListGroupItem>
-                            <ListGroupItem>My CityServices</ListGroupItem>
-                            <ListGroupItem>Setting</ListGroupItem>
-                        </ListGroup>
+                        <LeftProfileMenu userData={ this.props.userData }/>
                     </Col>
+
                     <Col md={1} xs={12} sm={12}>
                         <hr className='vertical' />
                     </Col>
+
                     <Col md={{ size: 7 }} sm={12}>
                         <Switch>
                             {
                                 profileRoute.map((route, i) => {
                                     if (this.props.match.url=='/profile'+route.path)
-                                        return <Route path={this.props.match.url} component={route.component} key={i} />;
+                                        return <Route exact path={this.props.match.url} component={route.component} key={i} />;
                                     else if (route.redirect===true)
                                         return <Route render={() => <Redirect to={route.to} />} key={i} />;
                                 })
                             }
                         </Switch>
                     </Col>
+
                 </Row>
             </Container>
         );
@@ -71,3 +56,28 @@ class Profile extends React.Component {
 
 
 export default connect(state => state)(Profile);
+
+
+
+const LeftProfileMenu = ({userData}) => (
+    <div>
+        <div className='profile-image'>
+            <img
+                src='https://pickaface.net/assets/images/slides/slide4.png'
+                width='200px'
+                className='img-fluid'
+                alt=''
+            />
+        </div>
+        <div className='profile-info'>
+            <strong>@{userData.userName}</strong>
+            <p>{userData.firstName} {userData.lastName}</p>
+        </div>
+        <ListGroup className='profile-menu'>
+            <ListGroupItem>Edit Profile</ListGroupItem>
+            <ListGroupItem>My DataCollections</ListGroupItem>
+            <ListGroupItem>My CityServices</ListGroupItem>
+            <ListGroupItem>Setting</ListGroupItem>
+        </ListGroup>
+    </div>
+)
