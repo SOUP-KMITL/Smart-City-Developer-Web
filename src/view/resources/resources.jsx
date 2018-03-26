c6676ea20a4e47def485dd190dc2a31471287ffc
Jitiwattana <bbos1@windowslive.com>
2018-03-26 20:14:53 +0700

    UPDATE link color of resources

--- src/view/resources/resources.jsx
+++ src/view/resources/resources.jsx
@@ -23,22 +23,22 @@ export default class Resources extends React.Component {
                             <ul>
                                 <li>
                                     <a href='https://drive.google.com/file/d/1l0MnyHMAqBmSUaYXPLd9R9lunPatC_gM/view?usp=sharing' target='_blank'>
-                                        <strong>* Augmented Reality & Virtual Reality (pptx)</strong> รายละเอียดการพัฒนา AR และ VR
+                                        <strong>* Augmented Reality & Virtual Reality (pptx)</strong><span className='black'> รายละเอียดการพัฒนา AR และ VR</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a href='https://docs.google.com/presentation/d/15jB4TTmPON8O3p65s8MgSWq0gvYJiby8T60URXnLv3U/edit?usp=sharing' target='_blank'>
-                                        <strong>* Storage Layer for Smart City Cloud Platform (GSlide)</strong> รายละเอียดทางเทคนิคของระบบเก็บข้อมูลที่ใช้ในแพลตฟอร์ม
+                                        <strong>* Storage Layer for Smart City Cloud Platform (GSlide)</strong><span className='black'> รายละเอียดทางเทคนิคของระบบเก็บข้อมูลที่ใช้ในแพลตฟอร์ม</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a href='https://drive.google.com/file/d/1ayezfXKCNKQUtT_LLAhISRdp2FdrdFxn/view?usp=sharing' target='_blank'>
-                                        <strong>* Introduction of Integrated Smart City Cloud Platform (pdf)</strong> ข้อมูลเบื้องต้นของแพลตฟอร์ม
+                                        <strong>* Introduction of Integrated Smart City Cloud Platform (pdf)</strong><span className='black'> ข้อมูลเบื้องต้นของแพลตฟอร์ม</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a href='https://docs.google.com/document/d/1T46xid3iNmINzKMJxnpqoOLMWun-owkdhuOUvl98k3E/edit?usp=sharing' target='_blank'>
-                                        <strong>* Progress Report on Storage Layer Development (GDoc)</strong> รายงานความคืบหน้าการพัฒนาระบบเก็บข้อมูล
+                                        <strong>* Progress Report on Storage Layer Development (GDoc)</strong><span className='black'> รายงานความคืบหน้าการพัฒนาระบบเก็บข้อมูล</span>
                                     </a>
                                 </li>
                             </ul>
